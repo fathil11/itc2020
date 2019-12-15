0723126a820cf1aee9ab4d47c88f6cf0d105c261
reas <andreasyulianto3@gmail.com>
2019-12-16 00:39:34 +0700

    controller method admin, observer, public

--- app/Http/Controllers/ObserverController.php
+++ app/Http/Controllers/ObserverController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class ObserverController extends Controller
 {
-    //
+    ///Participant Method
+    function showParticipantsTable(){}
+
+    function showAddParticipant(){}
+
+    function addParticipant(){}
+
+    function deleteParticipant(){}
+
+    ///Competition Method
+    function showAnswer(){}
+
+    function answer(){}
 }
--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
-    //
+    function showSoal(){}
+
+    function showPeserta(){}
 }
