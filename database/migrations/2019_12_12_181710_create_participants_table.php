48c259653bb1f6060d7ea2bdfdabe06d84e89e78
fathil11 <fathil.arham@gmail.com>
2019-12-15 21:32:10 +0700

    Initlialize Table Migration

---
+++ database/migrations/2019_12_12_181710_create_participants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',50);
+            $table->string('school',50);
+            $table->timestamp('absent',0);
+            $table->integer('point_1');
+            $table->integer('point_2');
+            $table->integer('point_3');
+            $table->integer('point_4');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
--- database/migrations/2019_12_12_181904_create_transaction_logs_table.php
+++ database/migrations/2019_12_12_181904_create_transaction_logs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTransactionLogsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transaction_logs', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->bigInteger('user_id');
+            $table->bigInteger('participant_id');
+            $table->bigInteger('question_id');
+            $table->char('answer', 2)->nullable();
+            $table->string('calc', 15);
         });
     }
 
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('transaction_logs');
     }
 }
