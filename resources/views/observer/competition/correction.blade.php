e561bf1c988d14c93fad39c1ca3ce4967db2ade3
fathil11 <fathil.arham@gmail.com>
2020-01-20 15:30:17 +0700

    change answer page

---
+++ resources/views/observer/competition/correction.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.observerLayout')
+@section('title', 'Koreksi Jawaban')
+@section('content')
+<div class="center">
+    @if ($question->session == 1)
+    <h4><b>Penyisihan 1</b></h4>
+    <h5 class="mt-1">Soal {{ $question->id }}</h5>
+    @elseif ($question->session == 2)
+    <h4><b>Penyisihan 2</b></h4>
+    <h5 class="mt-1">Soal {{ $question->id-30 }}</h5>
+    @elseif ($question->session == 3)
+    <h4><b>Final 1</b></h4>
+    <h5 class="mt-1">Soal {{ $question->id-50 }}</h5>
+    @elseif ($question->session == 4)
+    <h4><b>Final 2</b></h4>
+    <h5 class="mt-1">Soal {{ $question->id-60 }}</h5>
+    @else
+    @endif
+</div>
+<div class="divider mb-2"></div>
+<form method="post" action="{{ url('observer/competition/answer/') }}">
+    @csrf
+    @method('patch')
+    @foreach ($participants as $participant)
+    <div class="card">
+        <div class="card-content">
+            <div class="row">
+                <div class="col s3">
+                    <div class="btn-floating btn-large waves-effect waves-light secondary">
+                        <h5>{{ $participant->id }}</h5>
+                    </div>
+                </div>
+                <div class="col s9 mt-1">
+                    <h5 class="my-none secondary-text"><b>{{ $participant->name }}</b></h5>
+                </div>
+                <div class="col s12 mt-3 center">
+                    <p class="inline">
+                        <label>
+                            <input name="answer[{{ $participant->id }}]" type="radio" value="A" />
+                            <span class="blue-radio">A</span>
+                        </label>
+                    </p>
+                    <p class="inline">
+                        <label>
+                            <input name="answer[{{ $participant->id }}]" type="radio" value="B" />
+                            <span class="yellow-radio">B</span>
+                        </label>
+                    </p>
+                    @if ($question->session == 2 || $question->session == 3)
+                    <p class="inline">
+                        <label>
+                            <input name="answer[{{ $participant->id }}]" type="radio" value="C" />
+                            <span class="green-radio">C</span>
+                        </label>
+                    </p>
+                    @if ($question->session == 2)
+                    <p class="inline">
+                        <label>
+                            <input name="answer[{{ $participant->id }}]" type="radio" value="D" />
+                            <span>D</span>
+                        </label>
+                    </p>
+                    @endif
+                    @endif
+                    <p class="inline">
+                        <label>
+                            <input name="answer[{{ $participant->id }}]" type="radio" value="Z" />
+                            <span class="gray-radio">-</span>
+                        </label>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endforeach
+</form>
+<div class="mt-1 mb-bottom">
+    <button type="submit" class="btn btn-large btn-block">Masukan Jawaban</button>
+</div>
+
+@endsection
--- resources/views/observer/table.blade.php
+++ resources/views/observer/table.blade.php
@@ -2,59 +2,60 @@
 
 @section('content')
 <div class="container my-5">
-  <div class="row justify-content-center">
-    <div class="col-sm-10">
-      <h3>Data Peserta</h3>
+    <div class="row justify-content-center">
+        <div class="col-sm-10">
+            <h3>Data Peserta</h3>
+        </div>
     </div>
-  </div>
-  <div class="row justify-content-center">
-    <div class="col-sm-10">
-      <table class="table">
-        <thead class="thead-dark">
-          <tr>
-            <th scope="col">No</th>
-            <th scope="col">Nama</th>
-            <th scope="col">Sekolah</th>
-            <th scope="col">Status</th>
-            <th scope="col">Aksi</th>
-          </tr>
-        </thead>
-        <tbody>
-          @if (isset($participants))
-          @foreach ($participants as $participant)
-          <tr>
-            <td scope="row">{{ $participant->id }}</td>
-            <td scope="row">{{ $participant->name }}</td>
-            <td scope="row">{{ $participant->school }}</td>
-            <td scope="row">{{ $participant->status }}</td>
-            <td>
-                <form action="{{ url('observer/update/'.$participant->id) }}" method="post" class="d-inline">
-                    @csrf
-                    @method('post')
-                    <button class="btn waves-effect amber darken-2 waves-light tooltipped" data-position="top"
-                      data-tooltip="Update Peserta" type="submit" name="action">
-                      <i class="material-icons right">update</i>
-                    </button>
-                  </form>
-                  <form action="{{ url ('observer/table/'.$participant->id) }}" method="post"
-                    class="d-inline">
-                    @csrf
-                    @method('delete')
-                    <button class="btn waves-effect red waves-light tooltipped" data-position="top"
-                      data-tooltip="Delete Peserta" type="submit" name="action">
-                      <i class="material-icons right">delete</i>
-                    </button>
-                  </form>
-            </td>
-          </tr>
-          @endforeach
-          @endif
-        </tbody>
-      </table>
-      @if (isset($participant))
-      <a href="{{ url('observer/competition/answer/') }}" class="btn btn-primary my-3">Input Jawaban</a>
-      @endif
+    <div class="row justify-content-center">
+        <div class="col-sm-10">
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Sekolah</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @if (isset($participants))
+                    @foreach ($participants as $participant)
+                    <tr>
+                        <td scope="row">{{ $participant->id }}</td>
+                        <td scope="row">{{ $participant->name }}</td>
+                        <td scope="row">{{ $participant->school }}</td>
+                        <td scope="row">{{ $participant->status }}</td>
+                        <td>
+                            <form action="{{ url('observer/update/'.$participant->id) }}" method="post"
+                                class="d-inline">
+                                @csrf
+                                @method('post')
+                                <button class="btn waves-effect amber darken-2 waves-light tooltipped"
+                                    data-position="top" data-tooltip="Update Peserta" type="submit" name="action">
+                                    <i class="material-icons right">update</i>
+                                </button>
+                            </form>
+                            <form action="{{ url ('observer/table/'.$participant->id) }}" method="post"
+                                class="d-inline">
+                                @csrf
+                                @method('delete')
+                                <button class="btn waves-effect red waves-light tooltipped" data-position="top"
+                                    data-tooltip="Delete Peserta" type="submit" name="action">
+                                    <i class="material-icons right">delete</i>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                    @endif
+                </tbody>
+            </table>
+            @if (isset($participant))
+            <a href="{{ url('observer/competition/answer/') }}" class="btn btn-primary my-3">Input Jawaban</a>
+            @endif
+        </div>
     </div>
-  </div>
 </div>
 @endsection
